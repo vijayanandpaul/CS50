a4279e3815f68fc4b9a78ca58efa1b9a8419a360
vijayanandpaul@gmail.com <vijayanandpaul@gmail.com>
2022-03-02 07:51:34 -0800

    2022-03-02 changes

--- lecture5/counter.js
+++ lecture5/counter.js
@@ -1,13 +1,21 @@
-let counter = 0;
+// let counter = 0;
+
+if (!localStorage.getItem('counter')) {
+    localStorage.setItem('counter', 0);
+}
 
 function count(){
+    let counter = localStorage.getItem('counter');
     counter++;
     document.querySelector('h1').innerHTML = counter;
+    localStorage.setItem('counter', counter)
 
-    if (counter % 10 === 0) {
+/*     if (counter % 10 === 0) {
         alert(`Count is now ${counter}`);
-    }
+    } */
 }
 document.addEventListener('DOMContentLoaded', function() {
     document.querySelector('button').onclick = count;
+
+    setInterval(count, 1000);
 })
